e4e1180c2cab5077aee4c53fe652f33f6a42a7ce
baoduong <baoduong@littlelives.com>
2021-07-01 19:31:15 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CardGroup/>
+  </div>
+</template>
+
+<script>
+import CardGroup from './components/CardGroup.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CardGroup
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CardGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue";
+export default {
+  components: { Card },
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.container {
+  .hello {
+    color: red;
+  }
+}
+</style>
